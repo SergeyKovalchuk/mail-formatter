30b6b3ae19c5cc2eccc3c5eac26e806e95d6109b
Sergey_Kovalchuk <sergeyalexkoval@gmail.com>
2016-11-09 13:25:35 +0300

    add redux, router, user, and store configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -16,18 +19,34 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new NpmInstallPlugin()
   ],
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
     loaders: [
       {
-        loaders: ['babel-loader'],
+        loaders: ['react-hot', 'babel-loader'],
         include: [
           path.resolve(__dirname, "src"),
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   }
 }
